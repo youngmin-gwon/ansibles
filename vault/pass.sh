5575865b60ed786139e2ff8c9c4c2acb6b9a8ce9
Youngmin Gwon <youngmin.gwon@gmail.com>
2024-12-01 19:47:07 +0900

    fix(actions): add secrets for github actions

--- vault/pass.sh
+++ vault/pass.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-security find-generic-password -a "vault_pass" -s "ansible" -w
+
+if [ -z "${ANSIBLE_VAULT_PASSWORD+x}" ] || [ -z "$ANSIBLE_VAULT_PASSWORD" ]; then
+    export ANSIBLE_VAULT_PASSWORD=$(security find-generic-password -a "vault_pass" -s "ansible" -w)
+fi
+
+echo $ANSIBLE_VAULT_PASSWORD
--- vault/sudo_pass.sh
+++ vault/sudo_pass.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-security find-generic-password -a "sudo_pass" -s "ansible" -w
+
+if [ -z "${ANSIBLE_BECOME_PASSWORD+x}" ] || [ -z "$ANSIBLE_BECOME_PASSWORD" ]; then
+    export ANSIBLE_BECOME_PASSWORD=$(security find-generic-password -a "sudo_pass" -s "ansible" -w)
+fi
+
+echo $ANSIBLE_BECOME_PASSWORD
